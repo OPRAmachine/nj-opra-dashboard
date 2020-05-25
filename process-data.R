f46022a807482b22014f704c7abbaf7ce1b0ed98
Gavin Rozzi <gr@gavinrozzi.com>
2020-05-25 09:28:16 -0400

    Add logic for calculating OPRA response time
    The data processing script now uses lubridate to calculate the difference between the time a response was received and when the request was sent out, with the average value displayed on the dashboard

--- process-data.R
+++ process-data.R
@@ -1,19 +1,24 @@
 # Helper script to process request data for visualization
 library(tidyverse)
+library(lubridate)
 library(shiny)
 
-requests <- read.csv('data/opra_data_5-12-20.csv')
+requests <- read_csv('data/opra_data_5-12-20.csv')
 
 # Drop first column, not needed
 requests <- requests[ -c(1) ]
 
-#Reorder requests
+# Reorder requests in descending order
 requests <- requests %>% map_df(rev)
 
-# Create new output dataframe
-output <- requests %>% select(title, url_title, requested_by, requested_from, awaiting_description, described_state, request_created_at, tag_string)
+# Calculate time difference in days between last_public_response_at and request_created_at columns
+requests$days_until_response <- as.double(difftime(requests$last_public_response_at, requests$request_created_at),
+                                          units = "days")
 
-# Add the OPRAmachine URL to the url slug
+# Create new output dataframe
+output <- requests %>% select(title, url_title, requested_by, requested_from, awaiting_description, described_state, request_created_at, last_public_response_at, days_until_response, tag_string)
+
+# Add the OPRAmachine URL to the url slug to url_title column
 output$url_title <- paste('https://opramachine.com/request/', output$url_title, sep = "")
 
 # Create properly formatted HTML of data urls for the dashboard
@@ -25,7 +30,7 @@ for (i in 1:nrow(output)) {
 output <- rename(output, request = title_html)
 
 # Subset just the columns we need now that the tags have been generated
-output <- output %>% select(request, requested_by, requested_from, awaiting_description, described_state, request_created_at, tag_string)
+output <- output %>% select(request, requested_by, requested_from, awaiting_description, described_state, request_created_at, last_public_response_at, days_until_response, tag_string)
 
 # Write out to CSV
-write.csv(output, 'data/output.csv')
+write_csv(output, 'data/output.csv')
